7a6ec7d1fbfa4ec545625a40c14b1a1e0eb3f81d
laurenk104 <lkrieger104@gmail.com>
2025-04-16 13:27:18 -0700

    a lot of stuff

---
+++ public/widgets/menu/menu.js
@@ -0,0 +1,28 @@
+"use strict";
+(function() {
+  window.addEventListener("load", init)
+
+  function init() {
+    const container = document.querySelector("header");
+    container.innerHTML = ' \
+    <nav role="navigation" aria-label="main navigation"> \
+        <div> \
+          <a href="index.html">Fuzzy Feelings</a> \
+          <a></a> \
+        </div> \
+        <div class="navbar"> \
+          <a href="play.html">Play</a> \
+          <a href="learn.html">Learn</a> \
+          <a href="about.html">About Us</a> \
+          <a href="join.html"> \
+            <button id="join">Join Us</button> \
+          </a> \
+        </div> \
+      </nav>';
+
+    const styleSheet = document.createElement('link');
+    styleSheet.setAttribute("href", "../widgets/menu/menu.css");
+    styleSheet.setAttribute("rel", "stylesheet");
+    document.head.appendChild(styleSheet);
+  }
+})();
